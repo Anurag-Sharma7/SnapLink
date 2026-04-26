45b58b321e7652e3ed7447dc7fad807310ed8920
Anurag-Sharma7 <anuragprince03@gmail.com>
2026-04-26 13:46:47 +0530

    Mongo db access to all ipv4

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,13 @@ app.use("/", urlRoutes);
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log('Connected to MongoDB');
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    });
 }).catch((err) => {
   console.error('Error connecting to MongoDB:', err);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 
